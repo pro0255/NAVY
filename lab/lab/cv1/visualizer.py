ec5be8b8d21f588bfc325a6f07889e79bd674491
Vojtech Prokop <prokop.vojtech@gmail.com>
2021-02-20 11:51:39 +0100

    feat: vizualizer view

--- lab/lab/cv1/visualizer.py
+++ lab/lab/cv1/visualizer.py
@@ -4,9 +4,23 @@ from utils.equation import equation
 from lab.cv1 import CONSTANTS
 
 
+FIGURE_NAME = 'LineVisualizer' 
+PLOT_NAME = 'y = 4 * x - 5'
+
+
 class LineVisualizer:
     def __init__(self):
-        pass
+        self.fig = plt.figure(
+            num=FIGURE_NAME,
+            figsize=(20, 10),
+            dpi=80,
+            facecolor="w",
+            edgecolor="k",
+        )
+        self.ax = self.fig.add_subplot(111, title=PLOT_NAME)
+        self.legend=False
+        self.ax.grid(True)
+        
 
     def create_clusters(self, X, y_guess):
         x_points = X[:, 0]
@@ -36,9 +50,9 @@ class LineVisualizer:
 
     def draw_clusters(self, clusters):
         up, on, down = clusters
-        plt.scatter(up[0], up[1], c=CONSTANTS.COLOR_UP, label="nad")
-        plt.scatter(on[0], on[1], c=CONSTANTS.COLOR_ON, label="na")
-        plt.scatter(down[0], down[1], c=CONSTANTS.COLOR_DOWN, label="pod")
+        self.ax.scatter(up[0], up[1], c=CONSTANTS.COLOR_UP, label="nad")
+        self.ax.scatter(on[0], on[1], c=CONSTANTS.COLOR_ON, label="na")
+        self.ax.scatter(down[0], down[1], c=CONSTANTS.COLOR_DOWN, label="pod")
 
     def draw(self, X, y_guess):
         point_left = [CONSTANTS.LEFT_PTS, equation(CONSTANTS.LEFT_PTS)]
@@ -48,7 +62,12 @@ class LineVisualizer:
         y_values = [point_left[1], point_right[1]]
 
         self.draw_clusters(self.create_clusters(X, y_guess))
+        self.ax.plot(x_values, y_values, c="gray")
 
-        plt.plot(x_values, y_values, c="gray")
-        plt.legend()
-        plt.show()
+        if not self.legend:
+            self.legend = True
+            self.ax.legend()
+
+        plt.draw()
+        plt.pause(CONSTANTS.PAUSE_PLOT_TIME)
+
--- lab/main.py
+++ lab/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from lab.cv1.cv1 import cv1
+import matplotlib.pyplot as plt
+
 
 
 cv1()
