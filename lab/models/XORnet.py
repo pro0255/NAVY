d2f5a7c55e98abc6d56d8b730f24aa7cd93e9c88
Vojtech Prokop <prokop.vojtech@gmail.com>
2021-02-28 12:49:41 +0100

    feat: xor net dims

--- lab/models/XORnet.py
+++ lab/models/XORnet.py
@@ -1,7 +1,7 @@
 from lab.cv2 import CONSTANTS 
 import numpy as np
 from utils.sigmoid import sigmoid, sigmoid_
-from utils.error import error
+from utils.error import error, error2
 
 class XORnet:
     def __init__(self, learning_rate):
@@ -16,7 +16,7 @@ class XORnet:
     def fit(self, X, y):
         for i in range(CONSTANTS.NUMBER_OF_EPOCHS):
             self.forward_propagation(X)
-            self.back_propagation(X, y)
+            self.back_propagation(X, y, i%1000==0)
 
     def predict(self, X):
         self.forward_propagation(X)
@@ -26,27 +26,18 @@ class XORnet:
         self.out_H = sigmoid(X @ self.weights_H + self.bias_H)
         self.out_O = sigmoid(self.out_H @ self.weights_O + self.bias_O)
 
-
     #make comments
-    def back_propagation(self, X, y):
+    def back_propagation(self, X, y, debug_print=False):
         errors = error(y, self.out_O)
+        if debug_print:
+            print(errors, error2(y, self.out_O))
         delta_O = errors * sigmoid_(self.out_O)
         error_H = delta_O @ self.weights_O.T
         delta_H = error_H * sigmoid_(self.out_H)
 
-        self.weights_H = self.weights_H + X.T @ delta_H * self.learning_rate
-        self.weights_O = self.weights_O + X.T @ delta_O * self.learning_rate
+        self.weights_H = self.weights_H + (X.T @ delta_H) * self.learning_rate
+        self.weights_O = self.weights_O + (self.out_H.T @ delta_O) * self.learning_rate
 
-        self.bias_O += np.sum(delta_O,axis=0,keepdims=True) * self.learning_rate
+        self.bias_O += np.sum(delta_O, axis=0,keepdims=True) * self.learning_rate
         self.bias_H += np.sum(delta_H, axis=0, keepdims=True) * self.learning_rate
-
-
-
-
-
-        
-
-
-
-
 
--- lab/utils/error.py
+++ lab/utils/error.py
@@ -1,4 +1,8 @@
 import numpy as np
 
 def error(y, y_guess):
+    return y-y_guess
+    #return 1/2*np.power((y-y_guess), 2)
+
+def error2(y, y_guess):
     return 1/2*np.power((y-y_guess), 2)
